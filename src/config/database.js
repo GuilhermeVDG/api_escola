21914c1c3e59119271fa0a8f185675055471bf70
Guilherme Vanderley <guivdouradog@gmail.com>
2022-11-19 18:16:38 -0300

    init sequelize config and change to yarn

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'admin',
+  database: 'school',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
